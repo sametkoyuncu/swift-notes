78215fb9d0114650e6c3cc9207dcbaf04f5dfb8e
Samet Koyuncu <sametkoyuncu@live.com>
2022-08-30 23:13:32 +0300

    Add initial files

---
+++ dictionary.md
@@ -0,0 +1,23 @@
+- omit: Çıkarmak, atlamak.
+  > A significant detail was omitted from your story.
+- interpolation: İnterpolasyon, ekleme yapma, ara değeri bulma.
+- therefore: Öyleyse, bu yüzden, bu nedenle.
+  > He was injured and therefore unable to play.
+- injury: İncinme, hasar, zarar, yara.
+- appropriate: Uygun, yerinde, özgü.
+- deduce: Sonuç çıkarmak, anlamak.
+- inferry, inference: çıkarım
+- inferred: çıkarsanan, sonuç çıkarmak
+- explicitly: açıkça
+  > in a clear and detailed manner, leaving no room for confusion or doubt.
+  > "the essay should state explicitly how the facts support the thesis"
+- manner: tarz, tavır, tutum, davranış
+- compound: birleştirmek, bileşik, karışık
+- indead: aslında
+- individual: bireysel, tek, kişisel
+- distinct: belirgin, farklı, ayrı
+- suited: uygun, yeterli
+  > 1. right or appropriate for a particular person, purpose, or situation.
+  >    "the task is ideally suited to a computer"
+  > 2. wearing a suit of clothes of a specified type, fabric, or color.
+  >    "a dark-suited man"
---
+++ swiftBook.swift
@@ -0,0 +1,60 @@
+/*
+     ? Basics
+*/     
+//* Declaring Constants and Veriables
+let PI_NUMBER = 3.14 // constant - immutable
+var counter = 0 // mutable
+
+//* Type Annotations
+var welcomeMessage: String
+
+//* Printing Constants and Veriables
+print(welcomeMessage)
+
+//* or String Interpoletion
+print("Some string \(welcomeMessage)")
+
+//* Veriable Types
+    // Integers
+    // Int8, 16, 32, 64 / uint ...
+    // How to learn bound?
+    print("\(uint8.min) or \(uint16.max)")
+
+    // Floating - Point Numbers
+    // Float: 6 decimal digits
+    // Double: 15
+    // swift always chooces Double
+
+//* Numeric Literals
+    // no prefix: decimal numbers   -> 17
+    // 0b: binary notation          -> 0b10001
+    // 0o: octal notation           -> 0o21
+    // 0x: hexadecimal notation     -> 0x11
+    // 1.25e2 means 1.25 x 10**2
+    // 1.25e-2 means 1.25 x 10**-2
+
+//* Conversion
+var intValue: Int = 5
+var doubleValue = Double(intValue)
+
+//* Type Aliases
+    // Define an alternative name for an existing type.
+    typealias AudioSample = uint16
+    print(AudioSample.min) // print 0 because uint16
+
+//* Tuples
+    // Tuples group multiple values into a single compound value. The value within a tuple can be of anytype and don't have to be of the same type as each other.
+    let http404Error = (404, "page not found") // (int, string)
+    let (statusCode, statusMessage) = http404Error
+    print(statusMessage) // page not found
+    // or
+    let (statusCode, _) = http404Error // ignore the second item
+    // it likes object destructing in JS
+    // or you can use like this:
+    print(http404Error.0)
+    print(http404Error.1)
+    // or again. You can name the individual elements in a tuple when the tuple is defined.
+    let http200Ok = (statusCode: 200, statusMessage: "Ok")
+    // You can use the element names like this below
+    print(http200Ok.statusCode)
+    // ! Note: They (tuples) are not suited to the creation of complex data scructures.
