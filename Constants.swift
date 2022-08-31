2009ee8c7b181a298d5968e67a8b63ead8370403
Samet Koyuncu <sametkoyuncu@live.com>
2022-08-31 22:56:20 +0300

    Create Constants.swift

---
+++ Constants.swift
@@ -0,0 +1,25 @@
+// sample constants file
+//! K.swift
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
+ // usage
+print(K.cellIdentifier)
+print(K.BrandColors.purple)
