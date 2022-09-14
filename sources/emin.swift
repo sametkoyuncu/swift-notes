9da752034a7bffb988de73e21c25417b28c49bf7
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-14 22:05:14 +0300

    Update emin.swift

--- sources/emin.swift
+++ sources/emin.swift
@@ -1,4 +1,9 @@
- func fetchData() {
+// WebService.swift
+import Foundation
+
+class WebServices {
+ 
+     func fetchData() -> [Movie]? {
         let url = URL(string: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=464f8a5567ef6de84d256d195532ca13")
         URLSession.shared.dataTask(with: url!) {
             (data, response, error) in
@@ -7,15 +12,47 @@
                 if error == nil {
                     let jsonData =
                     try JSONDecoder().decode(DataModel.self, from: safeData)
-                    self.dataModel = jsonData.results
-                    DispatchQueue.main.async {
-                        self.myTable.reloadData()
-                    }
+                    return jsonData.results
+                 
                 }
             } catch {
                 print("Error")
+                return nil
             }
         }.resume()
-        
-        
     }
+}
+
+
+// ViewController.swift
+//
+//  ViewController.swift
+//  TestMovie
+//
+//  Created by Emin Saygı on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   // bu optional oldu
+    var dataModel = [Movie]?
+    // burası eklendi
+    var webService = WebService()
+
+    @IBOutlet weak var myTable: UITableView!
+    
+    
+    private var movieTableViewModel : TableViewCell!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        myTable.delegate = self
+        myTable.dataSource = self
+        
+        dataModel = webService.fetchData()
+     
+    }
+ // ....
+        
