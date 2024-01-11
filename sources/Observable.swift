ace4bb2d4fb2f3ca22a00f530afa83e67f584d0a
Samet Koyuncu <sametkoyuncu@live.com>
2024-01-11 11:30:27 +0300

    Update Observable.swift

--- sources/Observable.swift
+++ sources/Observable.swift
@@ -1,25 +1,23 @@
 class Observable<T> {
-  var value: T? {
-    didSet {
-      _callback?(value)
+    var value: T? {
+        didSet {
+            _callback?(value)
+        }
     }
-  }
-
-  private var _callback: ((T) -> Void)?
-
-  func bind(callback: @escaping (T?) -> Void) {
-    _callback = callback
+    
+    private var _callback: ((T) -> Void)?
+    
+    func bind(callback: @escaping (T?) -> Void) {
+        _callback = callback
+    }
 }
 
 class ViewModel {
-  var messageText: Observable<String> = Observable()
+    var messageText: Observable<String> = Observable()
 }
 
 class ViewController: UIViewController {
     viewModel.messageText.bind = { [weak self] value in
-                              self?.messageLabel.text = value
-                             }
+                                  self?.messageLabel.text = value
+                                 }
 }
-
-
-    
